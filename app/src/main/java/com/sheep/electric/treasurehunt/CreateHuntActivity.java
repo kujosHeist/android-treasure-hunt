84d7c518725c566c2d39a00d93755a9ebe334ccf
mcdonaldfergus <mcdonaldfergus@gmail.com>
2015-10-18 22:43:01 +0100

    added classes SelectTeamActivity and TeamOptionsActivity to add basic functionality to JoinHuntActivity

--- app/src/main/java/com/sheep/electric/treasurehunt/CreateHuntActivity.java
+++ app/src/main/java/com/sheep/electric/treasurehunt/CreateHuntActivity.java
@@ -1,10 +1,12 @@
 package com.sheep.electric.treasurehunt;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 public class CreateHuntActivity extends Activity {
 
@@ -37,4 +39,6 @@ public class CreateHuntActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
---
+++ app/src/main/java/com/sheep/electric/treasurehunt/SelectTeamActivity.java
@@ -0,0 +1,27 @@
+package com.sheep.electric.treasurehunt;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+/**
+ * Created by fergus on 18/10/2015.
+ */
+public class SelectTeamActivity extends Activity {
+
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_team);
+    }
+
+    public void selectTeam(View view){
+        Intent intent = new Intent(this, TeamOptionsActivity.class);
+        startActivity(intent);
+    }
+
+
+
+
+}
